ac8cd5197f2e437447992aa5964410e0efd07368
TCHOUKOUAHA Boris Gautier <btchoukouaha@univ-catho-sjd.com>
2024-06-03 12:22:22 +0100

    chore: Update database configuration with new password and port number

--- src/functions/db.config.ts
+++ src/functions/db.config.ts
@@ -4,8 +4,8 @@ const config: PoolConfig = {
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'position',
-  password: process.env.DB_PASSWORD || 'positionApi@2024',
-  port: parseInt(process.env.DB_PORT || '9801'),
+  password: process.env.DB_PASSWORD || 'postgres',
+  port: parseInt(process.env.DB_PORT || '5432'),
 };
 
 export const pool = new pg.Pool(config);
